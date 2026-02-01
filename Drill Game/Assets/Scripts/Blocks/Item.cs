db34f057994dc9616bd6713c92abab52b9aef2e3
swily123 <92625491+swily123@users.noreply.github.com>
2026-02-01 22:36:27 +0500

    improve selection animation

--- Drill Game/Assets/Scripts/Blocks/Item.cs
+++ Drill Game/Assets/Scripts/Blocks/Item.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
 using UnityEngine;
 
 namespace Blocks
@@ -6,17 +6,35 @@ namespace Blocks
     public class Item : MonoBehaviour
     {
         public bool IsCollected { get; private set; }
-        
-        public void Collect(Transform parent, Vector3 localPosition)
+    
+        public void Collect(Transform parent, Vector3 localTargetPosition, float duration = 0.3f)
         {
             if (IsCollected) return;
             IsCollected = true;
-    
-            if (TryGetComponent(out Collider col))
-                col.enabled = false;
-    
+        
+            Vector3 startWorldPos = transform.position;
+        
             transform.SetParent(parent);
-            transform.localPosition = localPosition;
+        
+            Vector3 startLocalPos = parent.InverseTransformPoint(startWorldPos);
+        
+            Vector3 peakLocalPos = (startLocalPos + localTargetPosition) / 2 + Vector3.up * 5f;
+
+            Vector3[] path = new Vector3[] 
+            { 
+                startLocalPos,
+                peakLocalPos,
+                localTargetPosition
+            };
+        
+            transform.localRotation = Quaternion.identity;
+            
+            transform.DOLocalPath(path, duration, PathType.CatmullRom)
+                .SetEase(Ease.OutQuad)
+                .OnComplete(() =>
+                {
+                    //TODO звук/эффекты
+                });
         }
     }
 }
