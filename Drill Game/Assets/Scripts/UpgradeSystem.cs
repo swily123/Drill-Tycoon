fba565a0ec26330a8d55e3e60e9b75cb84965051
swily123 <92625491+swily123@users.noreply.github.com>
2026-02-10 00:38:42 +0500

    added upgrade UI

---
+++ Drill Game/Assets/Scripts/UpgradeSystem.cs
@@ -0,0 +1,34 @@
+﻿using Planes;
+using UnityEngine;
+
+public class UpgradeSystem : MonoBehaviour
+{
+    [SerializeField] private PlaneButton _button;
+    [SerializeField] private Transform _upgradePanelUI;
+    
+    private bool _isActive;
+
+    private void Awake()
+    {
+        _isActive = false;
+        ToggleButton();
+    }
+
+    private void OnEnable()
+    {
+        _button.Clicked += ToggleButton;
+        _button.Released += ToggleButton;
+    }
+
+    private void OnDisable()
+    {
+        _button.Clicked -= ToggleButton;
+        _button.Released -= ToggleButton;
+    }
+
+    private void ToggleButton()
+    {
+        _upgradePanelUI.gameObject.SetActive(_isActive);
+        _isActive = !_isActive;
+    }
+}
